fa36d9f9dfa5fb1645b62d828c0d17e8b5067a36
NikolajPM <nikolajplagborg@gmail.com>
2015-03-06 21:12:45 -0600

    final version

--- 03 Visualizations/Categoricals.R
+++ 03 Visualizations/Categoricals.R
@@ -1,10 +1,10 @@
 myplot <- function(df, x) {
   names(df) <- c("x", "n")
-  ggplot(df, aes(x=x, y=n)) + geom_point()
+  ggplot(df, aes(x=x, y=n)) + geom_point() + theme(axis.text.x=element_text(angle=90, size=12, vjust=0.5))
 }
 
-}
 categoricals <- eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from INCOME"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_egc536', PASS='orcl_egc536' , MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE), 1, 2^31-1)))
+
 l <- list()
 for (i in names(income)) { 
   if (i %in% categoricals[[1]]) {
@@ -14,3 +14,12 @@ for (i in names(income)) {
     l[[i]] <- p
   }
 }
+
+png("../categoricals.png", width = 25, height = 10, units = "in", res = 72)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(1, 12)))   
+
+print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:4))
+print(l[[3]], vp = viewport(layout.pos.row = 1, layout.pos.col = 5:8))
+
+dev.off()
